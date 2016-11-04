73a284f9bd8dbe011886f505b1afdd7a9225eb3e
dmitriy <dmitriy.malec@gmail.com>
2016-11-04 11:09:45 +0200

    Added screen size and fixed test's view path

--- example/test/01_test_dummy.android.js
+++ example/test/01_test_dummy.android.js
@@ -6,15 +6,16 @@ const { driver, idFromXPath } = helper
 test('Test if user can see welcome message', async (t) => {
   const welcomeMessageId = idFromXPath(`
     //android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/
-    android.widget.FrameLayout[1]/android.view.ViewGroup[1]/
+    android.widget.FrameLayout[1]/android.view.View[1]/
     android.widget.TextView[1]
   `)
-
-  console.log('SOURCE:', await driver.getSource())
 
   await driver.waitForVisible(welcomeMessageId, 240000)
 
   t.pass('User should see welcome message')
 
-  console.log('SOURCE:', await driver.getSource())
+  await driver.waitForVisible(
+    idFromXPath('//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.view.View[1]/android.widget.TextView[3]'),
+    120000
+  )
 })
