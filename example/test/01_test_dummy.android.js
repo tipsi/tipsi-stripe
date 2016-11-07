c139869f5a81a0fdb5bb3c85b4a2bbc5ec50f7d2
Anton Petrov <petrov.aap@gmail.com>
2016-11-07 15:49:15 +0300

    Updated appium version, removed postinstall command

--- example/test/01_test_dummy.android.js
+++ example/test/01_test_dummy.android.js
@@ -6,7 +6,7 @@ const { driver, idFromXPath } = helper
 test('Test if user can see welcome message', async (t) => {
   const welcomeMessageId = idFromXPath(`
     //android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/
-    android.widget.FrameLayout[1]/android.view.ViewGroup[1]/
+    android.widget.FrameLayout[1]/android.view.View[1]/
     android.widget.TextView[1]
   `)
 
