44b0286f665c72dd59b044fd540345d6d1a2f2d1
Anton Petrov <petrov.aap@gmail.com>
2016-11-07 23:14:25 +0300

    Updated Travis script to configure project before build

--- .travis/ci.sh
+++ .travis/ci.sh
@@ -3,11 +3,13 @@
 case "${TRAVIS_OS_NAME}" in
   osx)
     cd example
+    npm run configure
     set -o pipefail && npm run build:ios | xcpretty -c -f `xcpretty-travis-formatter`
     npm run test:ios
   ;;
   linux)
     cd example
+    npm run configure
     npm run build:android
     npm run test:android
   ;;
