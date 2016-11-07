e1765dae564db13675b922cad7037cccb045bc15
Anton Petrov <petrov.aap@gmail.com>
2016-11-07 23:13:39 +0300

    Implemented Stripe singleton wrapper for native module

--- src/Stripe.ios.js
+++ src/Stripe.ios.js
@@ -1,5 +1,30 @@
 import { NativeModules } from 'react-native'
+import processTheme from './processTheme'
 
 const { TPSStripeManager } = NativeModules
 
-export default TPSStripeManager;
+class Stripe {
+  init = (options = {}) => (
+    TPSStripeManager.init(options)
+  )
+  deviceSupportsApplePay = () => (
+    TPSStripeManager.deviceSupportsApplePay()
+  )
+  paymentRequestWithApplePay = (items = [], options = {}) => (
+    TPSStripeManager.paymentRequestWithApplePay(items, options)
+  )
+  completeApplePayRequest = () => (
+    TPSStripeManager.completeApplePayRequest()
+  )
+  paymentRequestWithCardForm = (price = '0', options = {}) => (
+    TPSStripeManager.paymentRequestWithCardForm(price, {
+      ...options,
+      theme: processTheme(options.theme),
+    })
+  )
+  createTokenWithCard = (params = {}, options = {}) => (
+    TPSStripeManager.createTokenWithCard(params, options)
+  )
+}
+
+export default new Stripe()
