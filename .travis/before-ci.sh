e218cec2a9afb18b277bcb911ad5227f45e3ca7e
dmitriy <dmitriy.malec@gmail.com>
2016-11-24 00:57:00 +0200

    - little fix 2

--- .travis/before-ci.sh
+++ .travis/before-ci.sh
@@ -6,7 +6,7 @@ case "${TRAVIS_OS_NAME}" in
   ;;
   linux)
         echo no | android create avd --force -n test -t android-21 --abi armeabi-v7a --skin WVGA800
-    emulator -avd test -no-skin -no-audio -no-window &
+    emulator -avd test -scale 96dpi -dpi-device 160 -no-audio -no-window &
     android-wait-for-emulator
     sleep 60
     adb shell input keyevent 82 &
