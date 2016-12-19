2959e583e68f2cfd785da898840d6ff423dfa963
Anton Petrov <petrov.aap@gmail.com>
2016-12-19 22:45:44 +0700

    Travis possible fix

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,11 +4,15 @@ case "${TRAVIS_OS_NAME}" in
   osx)
     cd example_tmp
     npm install
+    pwd
+    ls -al
     react-native link
   ;;
   linux)
     cd example_tmp
     npm install
+    pwd
+    ls -al
     react-native link
   ;;
 esac
