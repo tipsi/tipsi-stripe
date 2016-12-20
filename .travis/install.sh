47e39a061f46aeea6bf13ba00692040200f6ae5e
Anton Petrov <petrov.aap@gmail.com>
2016-12-20 12:25:28 +0700

    Some fixes for Travis scripts

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,16 +4,12 @@ case "${TRAVIS_OS_NAME}" in
   osx)
     cd example_tmp
     npm install
-    pwd
-    ls -al
     react-native unlink tipsi-stripe
     react-native link
   ;;
   linux)
     cd example_tmp
     npm install
-    pwd
-    ls -al
     react-native unlink tipsi-stripe
     react-native link
   ;;
