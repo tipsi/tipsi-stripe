86f2ce3f17afb69ac113e20794f950ef8f4983e8
Sergey Zhukov <sergey.zhukov@komarov.mobi>
2017-03-10 11:40:39 +0400

    Solution for use_frameworks! CocoaPods directive (#38) Fixes #29, fixes #36
    * fix for ‘use_frameworks!’ directive in Podfile
    
    * changed wait time in tests

--- README.md
+++ README.md
@@ -530,6 +530,19 @@ You can fix it by installing `Carthage`:
 ```bash
 brew install carthage
 ```
+
+#### use_frameworks! issue
+
+If you are using `CocoaPods` and `use_frameworks!` enabled in your `Podfile` you might get the [following error](https://github.com/tipsi/tipsi-stripe/issues/29):
+
+`
+fatal error: 'Stripe/Stripe.h' file not found
+`
+
+To solve this problem please be sure that `Stripe.framework` is added to `Link Binary` with `Libraries` section of `Build Phases` in `TPSStripe.xcodeproj`. If problem still persist, please try to clean your build folder and rebuild again.
+
+![stripe_framework](https://cloud.githubusercontent.com/assets/1446268/23510226/1969c904-ff72-11e6-95b4-b918497b4d1b.png)
+
 #### Android
 
 * Using higher than [ours version](https://github.com/tipsi/tipsi-stripe/blob/master/android/build.gradle#L26) of Google Play Services in your project might encourage [an error](https://github.com/tipsi/tipsi-stripe/issues/18):
---
+++ scripts/post-link-ios.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+
+require 'xcodeproj'
+require 'tempfile'
+require 'fileutils'
+
+Dir.chdir('ios')
+
+@podfile_path = Pathname.pwd + 'Podfile'
+@pod_dep = "  pod 'Stripe', '~> 9.2.0'\n"
+
+@project_paths= Pathname.pwd.children.select { |pn| pn.extname == '.xcodeproj' }
+raise 'No Xcode project found' unless @project_paths.length > 0
+raise 'Multiple Xcode projects found' unless @project_paths.length == 1
+project_path = @project_paths.first
+
+project = Xcodeproj::Project.open(project_path)
+main_target = project.targets.first
+
+puts "Checking Podfile in iOS project #{@podfile_path}"
+
+
+if File.exist? @podfile_path 
+	puts 'Found an existing Podfile'
+	puts "Adding the following pod to Podfile\n#{@pod_dep}"
+	temp_file = Tempfile.new('Podfile_temp')
+	begin
+	  escaped_target_name = main_target.name.gsub(/'/, "\\\\\'")
+	  File.readlines(@podfile_path).each do |line|
+	  	if line =~ /pod\s'Stripe'/ 
+	  		puts 'Stripe pod is already added'
+	  		exit
+	  	end
+		temp_file.puts(line)
+	  	unless line =~ /^\s*(\#{0})use_frameworks!/ 
+	  		temp_file.puts(@pod_dep) if line =~ /target\s+'#{escaped_target_name}'\s+do/
+	  	else
+	  		puts 'Your project using use_frameworks! directive, please check README and issue https://github.com/tipsi/tipsi-stripe/issues/29'
+	  	end	  	
+	  end
+	  temp_file.close
+	  FileUtils.mv(temp_file.path, @podfile_path)
+	ensure
+		temp_file.delete
+	end
+else 	  
+	puts 'Adding Podfile to iOS project'
+	podfile = ''
+
+	podfile << "# Uncomment the next line to define a global platform for your project\n# platform :ios, '9.0'\n"
+	podfile << "\ntarget '#{main_target.name.gsub(/'/, "\\\\\'")}' do\n"
+	podfile << @pod_dep
+	podfile << "  inherit! :search_paths\n"
+	podfile << "end\n"
+	puts podfile
+	File.write(@podfile_path, podfile)
+end
+
+puts 'Installing Pods'
+
+system 'pod install'
