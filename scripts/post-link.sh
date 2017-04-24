8270baf5fa4fe5de836540c6de5a23cb2a0e60b0
Anton Kuznetsov <isnifer@gmail.com>
2017-04-24 09:25:45 +0300

    Rename post-link-android.sh -> scripts/post-link-android.js

--- scripts/post-link.sh
+++ scripts/post-link.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$(uname)" == "Darwin" ]; then
-	ruby $(dirname "$0")/post-link-ios.rb  
+	ruby $(dirname "$0")/post-link-ios.rb
 fi
 
-$(dirname "$0")/post-link-android.sh
+$(dirname "$0")/post-link-android.js
