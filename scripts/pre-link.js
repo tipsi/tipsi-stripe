016a2456664332d882d07a92f37db0611e0fb261
Anton Kuznetsov <isnifer@gmail.com>
2017-06-01 14:17:09 +0300

    Update pre-link.js

--- scripts/pre-link.js
+++ scripts/pre-link.js
@@ -1,4 +1,10 @@
+const os = require('os')
 const cp = require('child_process')
+
+if (os.type() !== 'Darwin') {
+  console.log('Skip linking tipsi-stripe for iOS')
+  return
+}
 
 console.log('Preparing to link tipsi-stripe for iOS')
 console.log('Checking CocoaPods...')
