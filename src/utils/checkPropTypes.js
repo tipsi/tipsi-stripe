7595599d6f08670c5372639fc02e4765a56dbaab
Anton Kuznetsov <isnifer@gmail.com>
2017-07-21 00:25:56 +0300

    Check publishableKey property on init method with prop-types. Fixes #73

--- src/utils/checkPropTypes.js
+++ src/utils/checkPropTypes.js
@@ -1,8 +1,10 @@
+import ReactPropTypesSecret from 'prop-types/lib/ReactPropTypesSecret'
+
 export default function checkPropTypes(typeSpecs, values, location, componentName) {
   if (process.env.NODE_ENV !== 'production') {
     for (var typeSpecName in typeSpecs) {
       if (typeSpecs.hasOwnProperty(typeSpecName)) {
-        const error = typeSpecs[typeSpecName](values, typeSpecName, componentName, location, null)
+        const error = typeSpecs[typeSpecName](values, typeSpecName, componentName, location, null, ReactPropTypesSecret)
         if (error instanceof Error) {
           throw new Error('Failed %s type: %s%s', location, error.message)
         }
