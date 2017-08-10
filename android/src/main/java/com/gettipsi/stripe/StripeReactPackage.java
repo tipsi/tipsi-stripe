5f2e275b485806e05bccb749497a09adf25dc725
Jordan Piggott <jordan1494@hotmail.com>
2017-08-10 18:24:08 +1000

    Fix for 0.47 Breaking Change with createJSModules Override (#100)
    * Fix for 0.47 Breaking Change with createJSModules Override
    
    * removed comments

--- android/src/main/java/com/gettipsi/stripe/StripeReactPackage.java
+++ android/src/main/java/com/gettipsi/stripe/StripeReactPackage.java
@@ -20,7 +20,6 @@ public class StripeReactPackage implements ReactPackage {
     return modules;
   }
 
-  @Override
   public List<Class<? extends JavaScriptModule>> createJSModules() {
     return Collections.emptyList();
   }
