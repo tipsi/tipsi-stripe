2db8b77332dfbdfcd1fc99d7abb4b9407569bacf
Andrey <paynd@users.noreply.github.com>
2017-09-11 10:00:31 +0300

    Check in every method that user called init method (#110)

--- scripts/pre-link.sh
+++ scripts/pre-link.sh
@@ -1,13 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Preparing to link tipsi-stripe for iOS"
+if [[ "$(uname)" == "Darwin" ]]; then
+  echo "Preparing to link tipsi-stripe for iOS"
 
-echo "Checking CocoaPods..."
-has_cocoapods=`which pod >/dev/null 2>&1`
-if [ -z "$has_cocoapods" ]
-then
-  echo "CocoaPods already installed"
-else
-  echo "Installing CocoaPods..."
-  gem install cocoapods
+  echo "Checking CocoaPods..."
+  has_cocoapods=`which pod >/dev/null 2>&1`
+  if [ -z "$has_cocoapods" ]
+  then
+    echo "CocoaPods already installed"
+  else
+    echo "Installing CocoaPods..."
+    gem install cocoapods
+  fi
 fi
--- src/Stripe.android.js
+++ src/Stripe.android.js
@@ -1,42 +1,56 @@
 import { NativeModules } from 'react-native'
 import checkArgs from './utils/checkArgs'
+import checkInit from './utils/checkInit'
 import * as types from './utils/types'
 
 const { StripeModule } = NativeModules
 
 class Stripe {
+  stripeInitialized = false
+
   init = (options = {}) => {
     checkArgs(
       types.initOptionsPropTypes,
       options, 'options', 'Stripe.init'
     )
+    this.stripeInitialized = true
     return StripeModule.init(options)
   }
-  deviceSupportsAndroidPay = () => (
-    StripeModule.deviceSupportsAndroidPay()
-  )
+
+  deviceSupportsAndroidPay = () => {
+    checkInit(this)
+    return StripeModule.deviceSupportsAndroidPay()
+  }
+
   paymentRequestWithAndroidPay = (options = {}) => {
+    checkInit(this)
     checkArgs(
       types.paymentRequestWithAndroidPayOptionsPropTypes,
       options, 'options', 'Stripe.paymentRequestWithAndroidPay'
     )
     return StripeModule.paymentRequestWithAndroidPay(options)
   }
+
   paymentRequestWithCardForm = (options = {}) => {
+    checkInit(this)
     checkArgs(
       types.paymentRequestWithCardFormOptionsPropTypes,
       options, 'options', 'Stripe.paymentRequestWithCardForm'
     )
     return StripeModule.paymentRequestWithCardForm(options)
   }
+
   createTokenWithCard = (params = {}) => {
+    checkInit(this)
     checkArgs(
       types.createTokenWithCardParamsPropTypes,
       params, 'params', 'Stripe.createTokenWithCard'
     )
     return StripeModule.createTokenWithCard(params)
   }
+
   createTokenWithBankAccount = (params = {}) => {
+    checkInit(this)
     checkArgs(
       types.createTokenWithBankAccountParamsPropTypes,
       params, 'params', 'Stripe.createTokenWithBankAccount'
