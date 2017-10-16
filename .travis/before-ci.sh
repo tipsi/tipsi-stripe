dc93e9e8b451a5c1e550304b0e780585a57d4078
Tony Holdstock-Brown <tony@cvstom.co>
2017-10-16 13:00:26 -0700

    Update travis according to travis#6801

--- .travis/before-ci.sh
+++ .travis/before-ci.sh
@@ -2,7 +2,6 @@
 
 case "${TRAVIS_OS_NAME}" in
   linux)
-    yes | $ANDROID_HOME/tools/bin/sdkmanager "build-tools;26.0.1"
     echo no | android create avd --force -n test -t android-21 --abi armeabi-v7a --skin WVGA800
     emulator -avd test -scale 96dpi -dpi-device 160 -no-audio -no-window &
     android-wait-for-emulator
