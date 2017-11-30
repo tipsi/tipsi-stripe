4f3bee88a6c6766fec1335e48d23a0e8e7c063e5
Anton Kuznetsov <isnifer@gmail.com>
2017-11-30 15:26:40 +0300

    Update .travis.yml

--- .travis/before-ci.sh
+++ .travis/before-ci.sh
@@ -2,13 +2,6 @@
 
 case "${TRAVIS_OS_NAME}" in
   linux)
-    case "${ANDROID_VERSION}" in
-      26)
-        echo y | android update sdk -a --no-ui --filter android-24
-        echo y | android update sdk -a --no-ui --filter sys-img-armeabi-v7a-android-24
-      ;;
-    esac
-
     echo no | android create avd --force -n test -t android-${ANDROID_VERSION} --abi armeabi-v7a --skin WVGA800
     emulator -avd test -scale 96dpi -dpi-device 160 -no-window &
     android-wait-for-emulator
