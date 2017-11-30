f1e14ad374d2281f72c50e6f3e2a1dbf3a4f021c
Anton Kuznetsov <isnifer@gmail.com>
2017-11-30 14:45:40 +0300

    Update .travis

--- .travis/before-ci.sh
+++ .travis/before-ci.sh
@@ -2,14 +2,7 @@
 
 case "${TRAVIS_OS_NAME}" in
   linux)
-    case "${ANDROID_VERSION}" in
-      21)
-        echo no | android create avd --force -n test -t android-${ANDROID_VERSION} --abi armeabi-v7a --skin WVGA800
-      ;;
-      26)
-        echo no | android create avd --force -n test -t android-${ANDROID_VERSION} --abi "google_apis/armeabi-v7a" --skin WVGA800
-      ;;
-    esac
+    echo no | android create avd --force -n test -t android-${ANDROID_VERSION} --abi armeabi-v7a --skin WVGA800
     emulator -avd test -scale 96dpi -dpi-device 160 -no-window &
     android-wait-for-emulator
     sleep 60
