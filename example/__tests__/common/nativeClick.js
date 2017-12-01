a77a19e7b8bddd67010499af7c6036385838df33
Anton Kuznetsov <isnifer@gmail.com>
2017-12-01 11:02:41 +0300

    Update nativeSwipe and nativeClick

--- example/__tests__/common/nativeClick.js
+++ example/__tests__/common/nativeClick.js
@@ -4,6 +4,6 @@ export default function nativeClick(x, y) {
   return new Promise((resolve, reject) => (
     exec(`adb shell input tap ${x} ${y}`, (error, stdout, stderr) => (
       error ? reject(error) : resolve(stdout)
-    )
+    ))
   ))
 }
--- example/__tests__/common/nativeSwipe.js
+++ example/__tests__/common/nativeSwipe.js
@@ -4,6 +4,6 @@ export default function nativeSwipe() {
   return new Promise((resolve, reject) => (
     exec('adb shell input touchscreen swipe 530 1420 530 1120', (error, stdout, stderr) => (
       error ? reject(error) : resolve(stdout)
-    )
+    ))
   ))
 }
