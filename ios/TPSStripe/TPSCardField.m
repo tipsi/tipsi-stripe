cb29e8cf3a7e99da2634431ba5d67d3fbcf82ace
Theo de Villeneuve <theodevilleneuve@gmail.com>
2018-12-14 17:56:31 +0100

    Remove NSNotificationCenter to fix ios lock with card input

--- ios/TPSStripe/TPSCardField.m
+++ ios/TPSStripe/TPSCardField.m
@@ -18,10 +18,6 @@
 
 }
 
-- (void)dealloc {
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:self.window];
-}
-
 - (instancetype)initWithFrame:(CGRect)frame {
     if ((self = [super initWithFrame:frame])) {
         _isFirstResponder = NO;
@@ -29,11 +25,6 @@
         _paymentCardTextField.delegate = self;
         [self addSubview:_paymentCardTextField];
         self.backgroundColor = [UIColor clearColor];
-        [[NSNotificationCenter defaultCenter]
-            addObserver:self
-               selector:@selector(keyboardWillShow:)
-                   name:UIKeyboardWillShowNotification
-                 object:self.window];
     }
     return self;
 }
