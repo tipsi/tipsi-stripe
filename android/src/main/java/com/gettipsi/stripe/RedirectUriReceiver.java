0366476359181b14dd712628d63d7f44847d4667
gal kahana <gal.kahana@hotmail.com>
2019-04-01 19:57:07 +0300

    oh come on

--- android/src/main/java/com/gettipsi/stripe/RedirectUriReceiver.java
+++ android/src/main/java/com/gettipsi/stripe/RedirectUriReceiver.java
@@ -2,7 +2,7 @@ package com.gettipsi.stripe;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
 /**
