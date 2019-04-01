7ccce92e8548bd74d9a070945cbda5c6ddcb1378
gal kahana <gal.kahana@hotmail.com>
2019-04-01 19:53:03 +0300

    oops

--- android/src/main/java/com/gettipsi/stripe/OpenBrowserActivity.java
+++ android/src/main/java/com/gettipsi/stripe/OpenBrowserActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 /**
  * Created by remer on 16/11/17.
