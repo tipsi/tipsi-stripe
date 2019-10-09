edc4b154aa42d070f98a51b524684af0e27b6801
Carlos Quiroga <carlos.quiroga.informatica@gmail.com>
2019-10-09 21:11:34 +0200

    AndroidX support

--- android/src/main/java/com/gettipsi/stripe/Errors.java
+++ android/src/main/java/com/gettipsi/stripe/Errors.java
@@ -1,6 +1,6 @@
 package com.gettipsi.stripe;
 
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 
 import com.facebook.react.bridge.ReadableMap;
 import com.gettipsi.stripe.util.ArgCheck;
