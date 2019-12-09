cdca29b8827a5b893ed3f0f185222d7f65a0a86e
Gregory <gregorygalushka@gmail.com>
2019-12-09 20:08:14 +0200

    catch NullPointerException
    I got ~40 crashes in my app on production cause of NullPointerException in this place. It's good first step to fix it.

--- android/src/main/java/com/gettipsi/stripe/RedirectUriReceiver.java
+++ android/src/main/java/com/gettipsi/stripe/RedirectUriReceiver.java
@@ -1,9 +1,9 @@
 package com.gettipsi.stripe;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
 
 /**
  * Created by remer on 11/8/17.
@@ -18,8 +18,14 @@ public class RedirectUriReceiver extends Activity {
       sendResult(RESULT_CANCELED);
     }
 
-    StripeModule.getInstance().processRedirect(getIntent().getData());
-    sendResult(RESULT_OK);
+    try {
+      Uri redirectData = getIntent().getData();
+
+      StripeModule.getInstance().processRedirect(redirectData);
+      sendResult(RESULT_OK);
+    } catch (NullPointerException error) {
+      sendResult(RESULT_CANCELED);
+    }
   }
 
   private void sendResult(int resultCode) {
