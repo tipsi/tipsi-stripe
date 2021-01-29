3a655ace64580f0bad46908df275d8b9ef55714a
KarlosQ <carlos.quiroga.informatica@gmail.com>
2021-01-29 16:28:25 +0100

    Use android X

--- android/src/main/java/com/gettipsi/stripe/StripeModule.java
+++ android/src/main/java/com/gettipsi/stripe/StripeModule.java
@@ -4,8 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import android.text.TextUtils;
 
 import com.facebook.react.bridge.ActivityEventListener;
